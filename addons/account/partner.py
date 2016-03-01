43ac73f76d8de5749342012edd80c4a44466b637
= <jesus@comunitea.com>
2016-03-01 10:06:01 -0500

    [IMP]Modificaciones en interfaz hechas por IV en el servidor.

--- addons/account/partner.py
+++ addons/account/partner.py
@@ -334,14 +334,14 @@ class res_partner(osv.osv):
             string="Account Payable",
             domain="[('type', '=', 'payable')]",
             help="This account will be used instead of the default one as the payable account for the current partner",
-            required=True),
+            required=False),
         'property_account_receivable': fields.property(
             type='many2one',
             relation='account.account',
             string="Account Receivable",
             domain="[('type', '=', 'receivable')]",
             help="This account will be used instead of the default one as the receivable account for the current partner",
-            required=True),
+            required=False),
         'property_account_position': fields.property(
             type='many2one',
             relation='account.fiscal.position',
